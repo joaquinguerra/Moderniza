3bfbf39a20bd5c33173ed1cbd85407166448afce
Joaquin Guerra <31641615+joaquinguerra@users.noreply.github.com>
2017-09-05 10:35:41 -0600

    subida de proyecto inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Button from 'react-md/lib/Buttons/Button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Button primary raised label="Hello, World!" />
+    );
+  }
+}
+
+export default App;
